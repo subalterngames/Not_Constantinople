b1aec7e594c2475abfeaf46bb2efa2f5f3da3027
Seth Alter <subalterngames@gmail.com>
2018-11-03 14:41:26 -0400

    Cleaned up some comments

--- not_constantine.py
+++ not_constantine.py
@@ -3,15 +3,29 @@ from json import loads
 import io
 from util import generate
 
+"""
+Create Markov-ified personal names from a blend of cultures.
+"""
 
-# Returns all provinces and their settlements with a given culture.
+
 def get_names_json():
+    """
+    Returns all provinces and their settlements with a given culture.
+    """
+
     with io.open(getcwd() + "/names.json", mode='rt', encoding="utf-8") as f:
         return loads(f.read())
 
 
-# Returns the names of a given key (male, female, surnames) a certain amount of times, given the input blend.
 def get_names(full_data, input_json, key):
+    """
+    Returns the names of a given key (male, female, surnames) a certain amount of times, given the input blend.
+
+    :param full_data: The entirety of the personal names JSON data.
+    :param input_json: The culture-blend input file.
+    :param key: Can be forenames_name, forenames_female, or surnames.
+    """
+
     names = []
     for culture in input_json["culture"].keys():
         if culture not in full_data.keys():
@@ -21,14 +35,15 @@ def get_names(full_data, input_json, key):
     return names
 
 
-names_data = get_names_json()
+if __name__ == "__main__":
+    names_data = get_names_json()
 
-# Load the stored cultural blends and generate some names.
-for f in listdir(getcwd() + "/Input"):
-    with io.open(getcwd() + "/Input/" + f, mode="rt", encoding="utf-8") as j:
-        # Get the saved cultural blend.
-        json_culture = loads(j.read())
-        # Generate names of provinces and settlements.
-        generate(json_culture["name"], "forenames_male", get_names(names_data, json_culture, "male"))
-        generate(json_culture["name"], "forenames_female", get_names(names_data, json_culture, "female"))
-        generate(json_culture["name"], "surnames", get_names(names_data, json_culture, "surnames"))
+    # Load the stored cultural blends and generate some names.
+    for f in listdir(getcwd() + "/Input"):
+        with io.open(getcwd() + "/Input/" + f, mode="rt", encoding="utf-8") as j:
+            # Get the saved cultural blend.
+            json_culture = loads(j.read())
+            # Generate names of provinces and settlements.
+            generate(json_culture["name"], "forenames_male", get_names(names_data, json_culture, "male"))
+            generate(json_culture["name"], "forenames_female", get_names(names_data, json_culture, "female"))
+            generate(json_culture["name"], "surnames", get_names(names_data, json_culture, "surnames"))
